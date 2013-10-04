9fcf1f0bf77bbfe8ab18b02d90734f873782269f
JamesShrout <shrjam22@ccm-2610-c1.evergreen.edu>
2013-10-04 13:25:33 -0700

    update of complete version

---
+++ hw1.py
@@ -0,0 +1,78 @@
+# Name: ...
+# Evergreen Login: ...
+# Computer Science Foundations
+# Programming as a Way of Life
+# Homework 1
+
+# You may do your work by editing this file, or by typing code at the
+# command line and copying it into the appropriate part of this file when
+# you are done.  When you are done, running this file should compute and
+# print the answers to all the problems.
+
+import math                     # makes the math.sqrt function available
+
+
+###
+### Problem 1
+###
+
+print "Problem 1 solution follows:"
+
+a= 1
+b= -5.86
+c= 8.5408
+x= (-b + math.sqrt (b*b - (4*a*c))) / (2*a)
+y= (-b - math.sqrt (b*b - (4*a*c))) / (2*a)
+print "the first root is"
+print x
+print "the second root is"
+print y
+
+
+
+###
+### Problem 2
+###
+
+print "Problem 2 solution follows:"
+
+import hw1_test
+
+print hw1_test.a
+print hw1_test.b
+print hw1_test.c
+print hw1_test.d
+print hw1_test.e
+print hw1_test.f
+
+
+###
+### Problem 3
+###
+
+print "Problem 3 solution follows:"
+
+if ((hw1_test.a and hw1_test.b) or (not hw1_test.c) and not (hw1_1.d or hw1_1.e or hw1_1.f)):
+	print "true"
+else :
+	print "false"
+
+
+###
+### Collaboration
+###
+
+
+
+	I had two tudors help me with this work on friday
+
+
+
+###
+### Reflection
+###
+	This work took me two and one half hours two complete with help from two tudors.
+I listened in class during lecture to learn the subject matter and I read from the downloadable book and I read from the tutorials.
+
+
+x
